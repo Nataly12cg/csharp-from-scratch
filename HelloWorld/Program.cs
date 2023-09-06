4e916a8eedf5000ae3f02a453c45b3e32d9a7a91
Nataly12cg <natalycorrea99@gmail.com>
2023-09-06 11:20:43 -0500

    charge hello world by class

--- HelloWorld/Program.cs
+++ HelloWorld/Program.cs
@@ -1,3 +1,7 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
-Console.ReadLine();
+﻿namespace HelloWorld{
+    class Program {
+        static void Main(){
+            Console.WriteLine("HelloWoeld");
+        }
+    }
+}
